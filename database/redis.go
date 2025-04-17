424e9531cb0cbc9b65d7cb84a5ec72abdf5665f7
clementus360 <clementus360@gmail.com>
2025-04-17 09:01:46 +0200

    parse redis host

--- database/redis.go
+++ database/redis.go
@@ -3,6 +3,8 @@ package database
 import (
 	"context"
 	"log"
+	"net/url"
+	"strconv"
 	"time"
 
 	"github.com/clementus360/proxy-chat/config"
@@ -13,23 +15,42 @@ var RedisClient *redis.Client
 
 func InitRedis() {
 	config.LoadEnv()
-	redisURL := config.GetEnv("REDIS_URL", "localhost:6379")
-	redisPassword := config.GetEnv("REDIS_PASSWORD", "")
-	redisDB := 0
+
+	rawURL := config.GetEnv("REDIS_URL", "localhost:6379")
+	password := config.GetEnv("REDIS_PASSWORD", "")
+	db := 0
+	addr := rawURL
+
+	// Check if REDIS_URL is a full URI like redis://...
+	if u, err := url.Parse(rawURL); err == nil && u.Scheme != "" {
+		addr = u.Host
+
+		if u.User != nil {
+			pw, _ := u.User.Password()
+			if pw != "" {
+				password = pw
+			}
+		}
+
+		if u.Path != "" && u.Path != "/" {
+			if dbNum, err := strconv.Atoi(u.Path[1:]); err == nil {
+				db = dbNum
+			}
+		}
+	}
 
 	RedisClient = redis.NewClient(&redis.Options{
-		Addr:     redisURL,
-		Password: redisPassword,
-		DB:       redisDB,
+		Addr:     addr,
+		Password: password,
+		DB:       db,
 	})
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	_, err := RedisClient.Ping(ctx).Result()
-	if err != nil {
+	if _, err := RedisClient.Ping(ctx).Result(); err != nil {
 		log.Fatalf("Unable to connect to redis: %v\n", err)
 	}
 
-	log.Println("Connected to redis")
+	log.Println("Connected to Redis at", addr)
 }
